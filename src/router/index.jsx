ad3785541ef78826f5e0c5dd7414d2129ea0a869
18580525352@163.com <18580525352@163.com>
2020-05-27 17:12:09 +0800

    feat: 补充

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import loadbale from '../utils/loadable';
--- src/utils/loadable.jsx
+++ src/utils/loadable.jsx
@@ -1,13 +1,20 @@
+import React from 'react';
 import Loadable from 'react-loadable';
+import '../style/public.css';
 
 /**
  * @description 通用的过场组件
+ * @param { 可选条件: props.error: 报错时, props.pastDelay: 如加载过慢启动过渡 }
  */
 const loadingComponent = (props) => {
   if (props.error) {
     return null;
   } else if (props.pastDelay) {
-    return null;
+    return (
+      <div className='load-box'>
+        <div className='loading'></div>
+      </div>
+    );
   } else {
     return null;
   }
